20c97fe6e0c6c9d713aa119352deed916bd6eebd
gnemes <gnemes@gmail.com>
2016-11-02 10:57:57 -0300

    Adding visits control

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -4,22 +4,26 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $this->saveVisit($request, "home");
         return $this->render('BlogBundle:Default:index.html.twig');
     }
     
     /**
      * @Route("/blog/post/{id}", name="blog_post")
      */
-    public function postAction($id)
+    public function postAction(Request $request, $id)
     {
+        $this->saveVisit($request, $id);
+        
         $viewName = $this->getPostViewById($id);
         
         if ($viewName == '') {
@@ -41,5 +45,20 @@ class DefaultController extends Controller
         }
         
         return $view;
+    }
+    
+    protected function saveVisit($request, $post)
+    {
+        $visit = new \BlogBundle\Entity\Visit();
+        $visit->setIp($request->getClientIp());
+        $visit->setPost($post);
+        
+        $em = $this->getDoctrine()->getManager();
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($visit);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();        
     }
 }
